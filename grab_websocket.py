d89d6462793c80b4281ab25a4cbc3cf5b9c34d83
Gabor Borbely <gaebor@gmail.com>
2021-04-10 10:51:06 +0200

    more robust websocket server

--- grab_websocket.py
+++ grab_websocket.py
@@ -6,12 +6,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
-import matplotlib.pyplot as plt
-import numpy
-
 from PIL import Image
-
-# plt.close(); plt.imshow(255-img); plt.show(block=False)
 
 
 class Application(tornado.web.Application):
@@ -20,26 +15,45 @@ class Application(tornado.web.Application):
 
 
 class MainHandler(tornado.websocket.WebSocketHandler):
+    frames = []
+
+    def check_frames():
+        expected = range(min(MainHandler.frames), max(MainHandler.frames) + 1)
+        if list(expected) != sorted(MainHandler.frames):
+            return set(expected) - set(MainHandler.frames)
+        else:
+            return "check_frames OK!"
+
     def on_message(self, message):
         text_message, image_data = message[:256], message[256:]
 
         text_message = text_message[: text_message.index(b'\0')].decode('utf8')
         try:
             text_message = json.loads(text_message)
+            if 'frame' in text_message:
+                MainHandler.frames.append(text_message['frame'])
+            elif text_message == "START":
+                MainHandler.frames = []
+            elif text_message == "END":
+                print(MainHandler.check_frames(), file=sys.stderr)
         except json.decoder.JSONDecodeError as e:
             print(e, sys.stderr)
 
         if len(image_data) > 0:
             image = Image.open(BytesIO(image_data), formats=['BMP'])
-
-        print(text_message)
+            # if 'frame' in text_message and text_message['frame'] % 100 == 0:
+            #     image.show()
+        else:
+            print(text_message)
 
 
 def main():
-    MainHandler.fig = plt.figure()
     app = Application()
     app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        tornado.ioloop.IOLoop.instance().stop()
 
 
 if __name__ == "__main__":
